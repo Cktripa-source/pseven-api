b8bb615d8df402e98b6c6146e1deaf48df5077d3
cktripa-source <websdeveloper2024@gmail.com>
2025-01-30 13:18:30 -0800

    Add service model

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const Service = require('../models/service');
+const Service = require('../models/service');  // Ensure this path is correct
 const router = express.Router();
 
 // Set up multer storage configuration
